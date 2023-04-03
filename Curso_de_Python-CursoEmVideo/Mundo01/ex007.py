e646e1935a58e9ac05b54318bd32133ce4040097
Jarbas de M. Ferreira Jr <72043035+jarbasmferreirajr@users.noreply.github.com>
2023-04-02 23:11:29 -0300

    Add files via upload
    Exercício 007 do curso de Python do "Curso em Vídeo do prof. Gustavo Guanabara".

---
+++ Curso_de_Python-CursoEmVideo/Mundo01/ex007.py
@@ -0,0 +1,10 @@
+print()
+print('===== Exercício 007 =====')
+print()
+n1 = float(input('Nota 01: '))
+n2 = float(input('Nota 02: '))
+s = n1 + n2
+m = s / 2
+print('A média do aluno é: {}'.format(m))
+print()
+print('FIM')
