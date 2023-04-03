cfb9a404937c75935367a056b25dee3c5aed576d
Jarbas de M. Ferreira Jr <72043035+jarbasmferreirajr@users.noreply.github.com>
2023-04-02 23:07:43 -0300

    Add files via upload
    Exercício 006 do curso de Python do "Curso em Vídeo do prof. Gustavo Guanabara".

---
+++ Curso_de_Python-CursoEmVideo/Mundo01/ex006.py
@@ -0,0 +1,7 @@
+print()
+print('===== Exercício 006 =====')
+print()
+n = int(input('Insira um número: '))
+print('Ok, o número que você inseriu é {}\nSeu dobro é {}\nSeu triplo é {}\nE sua raiz quadrada é {}.'.format(n, (n*2), (n*3), pow(n, (1/2))))
+print()
+print('FIM')
