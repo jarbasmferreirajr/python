1fd8f97bb35179c6729519f2aa698dc6858bfd26
Jarbas de M. Ferreira Jr <72043035+jarbasmferreirajr@users.noreply.github.com>
2023-04-02 23:06:35 -0300

    Add files via upload
    Exercício 005 do curso de Python do "Curso em Vídeo do prof. Gustavo Guanabara".

---
+++ Curso_de_Python-CursoEmVideo/Mundo01/ex005.py
@@ -0,0 +1,18 @@
+print()
+print('===== Exercício 005 =====')
+print()
+print('MODO 01')
+print()
+v = int(input('Insira um valor de entrada: '))
+a = v - 1
+s = v + 1
+print('Obrigado, seu número inserido foi {}, seu antecessor é {}, e o seu sucessor é {}.'.format(v, (v-1), (v+1)))
+print()
+print('FIM 1')
+print('........................................')
+print('MODO 02')
+print()
+v = int(input('Insira um valor de entrada: '))
+print('Obrigado, seu número inserido foi {}, seu antecessor é {}, e o seu sucessor é {}.'.format(v, (v-1), v+1))
+print()
+print('FIM 2')
