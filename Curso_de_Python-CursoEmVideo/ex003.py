6040ef613f020e067788865b18756d3fe0fae96c
Jarbas de M. Ferreira Jr <72043035+jarbasmferreirajr@users.noreply.github.com>
2023-04-02 22:50:52 -0300

    Add files via upload
    Exercício 003 do curso de Python do "Curso em Vídeo do prof. Gustavo Guanabara".

---
+++ Curso_de_Python-CursoEmVideo/ex003.py
@@ -0,0 +1,10 @@
+print()
+print('===== Exercício 003 =====')
+print()
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+print()
+print('A soma de {0} + {1} é igual a {2}!'.format(n1, n2, s))
+print()
+print("FIM")
