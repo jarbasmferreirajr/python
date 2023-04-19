13dbb442ed3840f078e8c0ce8b24a75c62359b7b
Jarbas de M. Ferreira Jr <72043035+jarbasmferreirajr@users.noreply.github.com>
2023-04-19 12:44:24 -0300

    ex006 - Exercicio-006
    ex006 - Exercicio-006.py

--- Curso_de_Python-CursoEmVideo/Mundo01/ex006.py
+++ Curso_de_Python-CursoEmVideo/Mundo01/ex006.py
@@ -5,3 +5,4 @@ n = int(input('Insira um número: '))
 print('Ok, o número que você inseriu é {}\nSeu dobro é {}\nSeu triplo é {}\nE sua raiz quadrada é {}.'.format(n, (n*2), (n*3), pow(n, (1/2))))
 print()
 print('FIM')
+
